0be85557efecf81554ded0dc144d24f16b3981f0
tidjaha <hamza.tidjani@yahoo.fr>
2025-01-30 10:46:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
-%%writefile app.py
 from IPython.display import HTML
 import streamlit as st
 import pandas as pd
 import joblib
-import matplotlib.pyplot as plt
+
 # Load the trained model
 model = joblib.load("model_final.pkl")
 
@@ -33,102 +31,102 @@ def predict(input_features):
 
 def main():
 
-    st.title("modele de prédiction du renouvelement d'un abonnement téléphonique")
+    st.title("Model for predicting the renewal of a phone subscription")
 
-    st.write("remplissez les champs pour avoir la prediction")
+    st.write("Fill in the fields to get the prediction")
 
     # Create input fields for user to enter data
 
-    tenure = st.number_input("combien de mois le client est resté (tenure)", min_value=1.0, step=1.0)
+    tenure = st.number_input("How many months has the customer been with us (tenure)", min_value=1.0, step=1.0)
     encodedtenure=scaler0.transform([[tenure]])[0][0]
     st.write(encodedtenure)
 
-    MultipleLines=st.selectbox("plusieurs lignes",["Yes","No","No phone service"])
+    MultipleLines=st.selectbox("Multiple phone lines ?",["Yes","No","No phone service"])
     encodedMultipleLines=encoder0.transform([MultipleLines])[0]
     st.write(encodedMultipleLines)
 
-    InternetService=st.selectbox("internet service",["DSL","Fiber optic","No"])
+    InternetService=st.selectbox("internet service ?",["DSL","Fiber optic","No"])
     encodedInternetService=encoder1.transform([InternetService])[0]
     st.write(encodedInternetService)
 
-    OnlineSecurity = st.selectbox("securité enligne",["Yes","No","No internet service"])
+    OnlineSecurity = st.selectbox("Online security ?",["Yes","No","No internet service"])
     encodedOnlineSecurity=encoder0.transform([OnlineSecurity])[0]
     st.write(encodedOnlineSecurity)
 
-    OnlineBackup = st.selectbox("sauvegarde enligne",["Yes","No","No internet service"])
+    OnlineBackup = st.selectbox("Online backup ?",["Yes","No","No internet service"])
     encodedOnlineBackup=encoder0.transform([OnlineBackup])[0]
     st.write(encodedOnlineBackup)
 
 
-    DeviceProtection = st.selectbox("protection des équipements",["Yes","No","No internet service"])
+    DeviceProtection = st.selectbox("Does he have equipment protection?",["Yes","No","No internet service"])
     encodedDeviceProtection=encoder0.transform([DeviceProtection])[0]
     st.write(encodedDeviceProtection)
 
-    TechSupport = st.selectbox("support technique",["Yes","No","No internet service"])
+    TechSupport = st.selectbox("Does he have TechSupport ?",["Yes","No","No internet service"])
     encodedTechSupport=encoder0.transform([TechSupport])[0]
     st.write(encodedTechSupport)
 
-    StreamingTV=st.selectbox('TV en streaming',["Yes","No","No internet service"])
+    StreamingTV=st.selectbox('Streaming TV ?',["Yes","No","No internet service"])
     encodedStreamingTV=encoder0.transform([StreamingTV])[0]
     st.write(encodedStreamingTV)
 
-    StreamingMovies=st.selectbox('Film en streaming',["Yes","No","No internet service"])
+    StreamingMovies=st.selectbox('Streaming Movies ?',["Yes","No","No internet service"])
     encodedStreamingMovies=encoder0.transform([StreamingMovies])[0]
     st.write(encodedStreamingMovies)
 
-    Contract=st.selectbox('type de contract',["Month-to-month","One year","Two year"])
+    Contract=st.selectbox('Type of contract ?',["Month-to-month","One year","Two year"])
     encodedContract=encoder2.transform([Contract])[0]
     st.write(encodedContract)
 
-    PaymentMethod=st.selectbox('type de paiement',["Electronic check","Mailed check","Bank transfer (automatic)","Credit card (automatic)"])
+    PaymentMethod=st.selectbox('Payment method ?',["Electronic check","Mailed check","Bank transfer (automatic)","Credit card (automatic)"])
     encodedPaymentMethod=encoder3.transform([PaymentMethod])[0]
     st.write(encodedPaymentMethod)
 
 
-    MonthlyCharges = st.number_input("les charges mensuelle", min_value=0.0)
+    MonthlyCharges = st.number_input("Monthly charges (amount) ?", min_value=0.0)
     encodedMonthlyCharges=scaler1.transform([[MonthlyCharges]])[0][0]
     st.write(encodedMonthlyCharges)
 
-    TotalCharges = st.number_input("totalité des charges", min_value=0.0)
+    TotalCharges = st.number_input("Total charges (amount) ?", min_value=0.0)
     encodedTotalCharges=scaler2.transform([[TotalCharges]])[0][0]
     st.write(encodedTotalCharges)
 
-    gender_Male=st.selectbox('Genre',['Homme','Femme'])
+    gender_Male=st.selectbox('Gender',['Homme','Femme'])
     if gender_Male=="Homme":
        encodedgender_Male=True
     else:
        encodedgender_Male=False
     st.write(encodedgender_Male)
 
-    Partner_Yes=st.selectbox('es tu un patenaire ?',['Yes', 'No'])
+    Partner_Yes=st.selectbox('Is he a partner?',['Yes', 'No'])
     if Partner_Yes=="Yes":
        encodedPartner_Yes=True
     else:
        encodedPartner_Yes=False
     st.write(encodedPartner_Yes)
 
-    Dependents_Yes=st.selectbox('es tu dependant ?',['Yes', 'No'])
+    Dependents_Yes=st.selectbox('Is he dependent?',['Yes', 'No'])
     if Dependents_Yes=="Yes":
        encodedDependents_Yes=True
     else:
        encodedDependents_Yes=False
     st.write(encodedDependents_Yes)
 
-    PhoneService_Yes=st.selectbox('as tu un service téléphonique ?',['Yes', 'No'])
+    PhoneService_Yes=st.selectbox('Does he have a telephone service?',['Yes', 'No'])
     if PhoneService_Yes=="Yes":
        encodedPhoneService_Yes=True
     else:
        encodedPhoneService_Yes=False
     st.write(encodedPhoneService_Yes)
 
-    PaperlessBilling_Yes=st.selectbox('facture electronique ?',['Yes', 'No'])
+    PaperlessBilling_Yes=st.selectbox('Paperless billing ?',['Yes', 'No'])
     if PaperlessBilling_Yes=="Yes":
        encodedPaperlessBilling_Yes=True
     else:
        encodedPaperlessBilling_Yes=False
     st.write(encodedPaperlessBilling_Yes)
 
-    SeniorCitizen_1=st.selectbox('es tu un senior ?',['Yes', 'No'])
+    SeniorCitizen_1=st.selectbox('Is he a senior citizen ?',['Yes', 'No'])
     if SeniorCitizen_1=="Yes":
        encodedSeniorCitizen_1=True
     else:
@@ -155,12 +153,12 @@ def main():
         if predictions==False:
           emoji='''<div style="font-size: 200px; text-align: center;">😍</div>'''
           st.markdown(emoji, unsafe_allow_html=True)
-          text = """<div style="text-align: center; font-size: 60px;color: green;">Bavo ! il sera un nouveau client ou va continuer l'aventure</div>"""
+          text = """<div style="text-align: center; font-size: 60px;color: green;">Congratulation ! He will either become a new customer or continue with us.</div>"""
           st.markdown(text, unsafe_allow_html=True)
         else:
           emoji='''<div style="font-size: 200px; text-align: center;">😅</div>'''
           st.markdown(emoji, unsafe_allow_html=True)
-          text = """<div style="text-align: center; font-size: 60px;color: red;">Il faut revoir l'offre, car l'abonné n'a pas l'intention de renouveler</div>"""
+          text = """<div style="text-align: center; font-size: 60px;color: red;">The offer needs to be reviewed, as the subscriber has no intention of renewing.</div>"""
           st.markdown(text, unsafe_allow_html=True)
 
 
